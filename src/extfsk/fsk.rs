fb73e563a1057c871973f17017458716c2883154
Yū KOBAYASHI <kb10uy@kb10uy.org>
2026-01-15 14:10:50 +0900

    FSK is currently unsupported

--- src/extfsk/fsk.rs
+++ src/extfsk/fsk.rs
@@ -1,9 +1,9 @@
 use crate::core::{config::Config, error::Result};
 
-pub fn is_busy(config: &Config) -> Result<bool> {
+pub fn is_busy(_config: &Config) -> Result<bool> {
     Ok(false)
 }
 
-pub fn put_char(config: &Config, c: u8) -> Result<()> {
+pub fn put_char(_config: &Config, _c: u8) -> Result<()> {
     Ok(())
 }
